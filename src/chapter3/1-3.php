f7d9ac857009aca338c4da8bb7e171ba2fb4383a
dukken06 <kenxavi.8@gmail.com>
2023-08-24 15:01:42 +0900

    chapter3作成

---
+++ src/chapter3/1-3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>chapter3</title>
+</head>
+<body>
+  <?php
+    echo 10 * 4 + 5 - 2;
+    echo '<br>';
+    echo 40 / 8;
+    echo '<br>';
+    echo 7 % 2;
+    $x = 10 * 4 + 5 - 2;
+    echo '<br>';
+    echo $x;
+    echo '<br>';
+    echo $x * 2;
+    echo '<br>';
+    echo $x * 3;
+    echo '<br>';
+    echo $x * 4;
+    echo '<br>';
+    echo 10;
+    echo '<br>';
+    echo '10';
+  ?>
+</body>
+</html>
