458bc2521d4af022e3fa9107c777cc43da8eebfb
dukken06 <kenxavi.8@gmail.com>
2023-08-29 10:03:35 +0900

    chapter4作成

---
+++ src/chapter4/1-4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>chapter4</title>
+</head>
+<body>
+  <?php
+    echo '文字列';
+    echo '<br>';
+    echo 3 + 5;
+    echo '<br>';
+    $x = 'こんにちは、'.'チャンネル登録してね';
+    echo $x;
+    echo '<br>';
+    $x = '最'.'&'.'高';
+    echo $x;
+    echo '<br>';
+    $x = 0.1 + 0.2 - 0.5;
+    echo $x;
+    echo '<br>';
+    echo 100 - 99.6;
+  ?>
+</body>
+</html>
